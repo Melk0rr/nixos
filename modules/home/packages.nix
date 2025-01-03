43e1b11cee9e4d4c0de2990bdb4eeaf68d9d832e
me1k0r <runner-me1k0r@proton.me>
2025-01-03 21:40:37 +0100

    edit: home - missioncenter

--- modules/home/packages.nix
+++ modules/home/packages.nix
@@ -23,6 +23,7 @@
 
     # Utility
     localsend			# foss alternative to airdrop
+    missioncenter		# gtk app to track system resources
     mousam			# gtk weather app
     protonmail-desktop		# proton mail desktop client
     proton-pass			# proton pass desktop client
