9cf1a09762efd025e1781c0613fef955d1e1f782
me1k0r <runner-me1k0r@proton.me>
2025-01-21 17:12:18 +0100

    edit: home - neovim lualine config

--- modules/home/nvim/lua/user/mappings.lua
+++ modules/home/nvim/lua/user/mappings.lua
@@ -1,33 +1,42 @@
-local map = vim.api.nvim_set_keymap
+local map = vim.keymap.set
+local opts = { noremap = true, silent = true }
+
 -- Common
-map('', 'Y', 'y$', {})                                      -- 'Y' to yank the whole line
-map('i', 'kj', '<esc>', {})                                 -- kj to exit insert mode
-map('n', '<leader;', 'mqA;<esc>`q', {})                     -- put semicolon at the end of the line
+map('', 'Y', 'y$', opts)                                      -- 'Y' to yank the whole line
+map('n', '<leader;', 'mqA;<esc>`q', opts)                     -- put semicolon at the end of the line
+
+-- Press jk fast to exit insert mode
+map('i', 'jk', '<ESC>', opts)
+map('i', 'kj', '<ESC>', opts)
 
 -- Window navigation
-map('n', '<C-h>', '<C-w>h', {})
-map('n', '<C-j>', '<C-w>j', {})
-map('n', '<C-k>', '<C-w>k', {})
-map('n', '<C-l>', '<C-w>l', {})
-map('t', '<C-h>', [[<C-\><C-n><C-w>h]], {})
-map('t', '<C-j>', [[<C-\><C-n><C-w>j]], {})
-map('t', '<C-k>', [[<C-\><C-n><C-w>k]], {})
-map('t', '<C-l>', [[<C-\><C-n><C-w>l]], {})
+map('n', '<C-h>', '<C-w>h', opts)
+map('n', '<C-j>', '<C-w>j', opts)
+map('n', '<C-k>', '<C-w>k', opts)
+map('n', '<C-l>', '<C-w>l', opts)
+map('t', '<C-h>', [[<C-\><C-n><C-w>h]], opts)
+map('t', '<C-j>', [[<C-\><C-n><C-w>j]], opts)
+map('t', '<C-k>', [[<C-\><C-n><C-w>k]], opts)
+map('t', '<C-l>', [[<C-\><C-n><C-w>l]], opts)
 
 -- Buffer navigation
-map('n', '<Tab>', '<cmd>bnext<cr>', {})
-map('n', '<S-Tab>', '<cmd>bprevious<cr>', {})
+map('n', '<Tab>', '<cmd>bnext<cr>', opts)
+map('n', '<S-Tab>', '<cmd>bprevious<cr>', opts)
+--
+-- Stay in indent mode
+map('v', '<', '<gv^', opts)
+map('v', '>', '>gv^', opts)
 
 -- Move lines
-map('n', '<A-j>', ':m .+1<cr>==', {})
-map('n', '<A-k>', ':m .-2<cr>==', {})
-map('v', '<A-j>', ":m '>+1<cr>gv=gv", {})
-map('v', '<A-k>', ":m '<-2<cr>gv=gv", {})
+map('n', '<A-j>', ':m .+1<cr>==', opts)
+map('n', '<A-k>', ':m .-2<cr>==', opts)
+map('v', '<A-j>', ":m '>+1<cr>gv=gv", opts)
+map('v', '<A-k>', ":m '<-2<cr>gv=gv", opts)
 
 -- Plugin-specific
 local telescope = require 'telescope.builtin'
-vim.keymap.set('n', '<C-p>', telescope.find_files, {})
-vim.keymap.set('n', '<leader>fg', telescope.live_grep, {})
+map('n', '<C-p>', telescope.find_files, opts)
+map('n', '<leader>fg', telescope.live_grep, opts)
 
 -- neo-tree
-vim.keymap.set('n', '<leader>e', '<cmd>Neotree toggle<cr>', {})
+map('n', '<leader>e', '<cmd>Neotree toggle<cr>', opts)
--- modules/home/nvim/lua/user/plugins/init.lua
+++ modules/home/nvim/lua/user/plugins/init.lua
@@ -4,6 +4,7 @@ return {
   require("user.plugins.bufferline"),
   require("user.plugins.catppuccin"),
   require("user.plugins.lspconfig"),
+  require("user.plugins.lualine"),
   require("user.plugins.neotree"),
   require("user.plugins.telescope"),
   require("user.plugins.toggleterm"),
