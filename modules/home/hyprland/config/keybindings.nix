e16e409a98c5b9d7fd6dd1e33c2e3a7c9fa4f513
me1k0r <runner-me1k0r@proton.me>
2025-01-26 18:54:59 +0100

    edit: home - hypr keybindings base

---
+++ modules/home/hyprland/config/keybindings.nix
@@ -0,0 +1,89 @@
+{
+  wayland.windowManager.hyprland.settings = {
+    "$mainMod" = "SUPER";
+    
+    # assign apps
+    "$term" = "kitty";
+    #"$editor" = "codium --ozone-platform-hint=wayland --disable-gpu";
+    "$file" = "yazi";
+    "$filegui" = "dolphin";
+    "$browser" = "zen-browser";
+    "$ytclient" = "freetube --ozone-platform-hint=wayland";
+    #"$music" = "kitty --session music.session";
+    "$musicgui" = "lollypop";
+    "$sysmon" = "missioncenter";
+
+    # Classical bindings
+    bind = {
+      
+    };
+
+    # Bindings with description
+    bindd = {
+      # Window/Session actions
+      "$mainMod, killactive, kill the window on focus, Q, killactive"
+      "ALT, F4, killactive, kill the window on focus, killactive"
+      "$mainMod, Kill hyperland session, delete, exit,"
+      "$mainMod, W, Toggle the window on focus to float, togglefloating,"
+      "ALT, return, Toggle fullscreen for the focused window, fullscreen,"
+      "$mainMod, L, Lock screen, exec, hyprlock"
+      "$mainMod SHIFT, F, Toggle pin on focused window, exec, windowpin"
+      "$mainMod, backspace, Logout menu, exec, logoutlaunch 2"
+      "$CONTROL, ESCAPE, Toggle waybar, exec, killall waybar || waybar"
+
+      # Groups
+      "$mainMod, G, Toggle the window on focus to group (tab mode), togglegroup,"
+      "$mainMod, F2, Next window in group, changegroupactive, f"
+      "$mainMod, F1, Previous window in group, changegroupactive, b"
+
+      # Application shortcuts
+      "$mainMod, RETURN, Open terminal, exec, $term"
+      "$mainMod, E, Open GUI file manager, exec, $filegui"
+      "$mainMod ALT, E, Open TUI file manager, exec, kitty -e $file"
+      "$mainMod, C, Open code editor, exec, $editor"
+      "$mainMod, F, Open FreeTube, exec, $ytclient"
+      "$mainMod, B, Open browser, exec, $browser"
+      "$mainMod SHIFT, M, Open TUI music player, exec, killall cmus && killall cava || $music"
+      "$mainMod, M, Open GUI music player, exec, $musicgui"
+      "$CONTROL SHIFT, ESCAPE, Open system monitor, exec, $sysmon"
+
+      # Rofi is toggled on/off if you repeat the key presses
+      "$mainMod, A, Browse system files, exec, pkill -x rofi || rofilaunch f"
+      "$mainMod, tab, Switch between desktop applications, exec, pkill -x rofi || rofilaunch w"
+      "$mainMod, R, Launch desktop applications, exec, pkill -x rofi || rofilaunch d"
+
+      # Screenshot/Screencapture
+      "$mainMod, P, Drag to screenshot, exec, screenshot s"
+      "$mainMod SHIFT, P, Triggers color picker, exec, hyprpicker -a"
+      "$mainMod CTRL, P, frozen screen / drag screenshot, exec, screenshot sf"
+      "$mainMod ALT, P, Print focused monitor, exec, screenshot m"
+      ",print, Take a screenshot of all monitor, exec, screenshot p"
+    };
+
+    # Bindings with description and working with an input inhibitor (e.g. lockscreen)
+    binddl = {
+      # Audio control
+      ", F10, Toggle audio mute, exec, volumecontrol -o m"
+      ", XF86AudioMute, Toggle audio mute, exec, volumecontrol -o m"
+      ", XF86AudioMicMute, Toggle microphone mute, exec, volumecontrol -i m"
+      ", XF86AudioPlay, Play media, exec, playerctl play-pause"
+      ", XF86AudioPause, Pause media, exec, playerctl play-pause"
+      ", XF86AudioNext, Next media track, exec, playerctl next"
+      ", XF86AudioPrev, Previous media track, exec, playerctl previous"
+
+      # Brightness control
+      "$mainMod, F10, Display brightness, exec, brightnesscontrol g"
+      "$mainMod, F11, Decrease brightness, exec, brightnesscontrol d"
+      "$mainMod, F12, Increase brightness, exec, brightnesscontrol i"
+    };
+
+    # Bindings with description, working with input inhibitor and will repeat when held
+    binddel = {
+      # Audio control
+      ", F11, Decrease volume, exec, volumecontrol -o d"
+      ", F12, Increase volume, exec, volumecontrol -o i"
+      ", XF86AudioLowerVolume, Decrease volume, exec, volumecontrol -o d"
+      ", XF86AudioRaiseVolume, Increase volume, exec, volumecontrol -o i"
+    };
+  };
+}
