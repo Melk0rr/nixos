e35304afdf17e30b1879dea9d3a6f6c74907eaa4
me1k0r <runner-me1k0r@proton.me>
2025-02-03 11:51:49 +0100

    new: home - catppuccin config

---
+++ modules/home/catppuccin.nix
@@ -0,0 +1,19 @@
+{ pkgs, ... }:
+
+# Catppuccin theme config
+{
+  home.packages = (
+    with pkgs; [ catppuccin ]
+  )
+
+  gtk = {
+    enable = true;
+    catppuccin = {
+      enable = true;
+      flavor = "mocha";
+      accent = "pink";
+      size = "standard";
+      tweaks = [ "normal" ];
+    };
+  };
+}
