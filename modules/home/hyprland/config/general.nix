980991563d2ab3a7d7b554434a59359040d5a3b8
me1k0r <runner-me1k0r@proton.me>
2025-02-08 22:35:34 +0100

    fix: modules/home/hyprland/config - general.nix
    dwindle layout between quotes

--- modules/home/hyprland/config/general.nix
+++ modules/home/hyprland/config/general.nix
@@ -5,7 +5,7 @@
     gaps_out = 15;
     border_size = 2;
     resize_on_border = true;
-    layout = dwindle;
+    layout = "dwindle";
 
     "col.active_border" = "rgba(f5e0dca7) rgba(eba0aca7) 45deg";
     "col.inactive_border" = "rgba(f5e0dc17) rgba(eba0ac17) 45deg";
