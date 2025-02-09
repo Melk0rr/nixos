3336c518a2c2db97eead11f17da56951fa8a7b01
me1k0r <runner-me1k0r@proton.me>
2025-02-09 22:39:54 +0100

    edit: modules/home - gtk.nix
    changed package

--- modules/home/gtk.nix
+++ modules/home/gtk.nix
@@ -7,19 +7,30 @@
       size = 12;
     };
     theme = {
-      name = "Catppuccin-Mocha-Standard-Lavender-dark";
-      package = pkgs.catppuccin-gtk.override {
-        accents = [ "pink" ];
+      name = "Catppuccin-Dark";
+      package = pkgs.magnetic-catppuccin-gtk.override {
+        accent = [ "pink" ];
         size = "standard";
-        variant = [ "mocha" ];
       };
     };
     iconTheme = {
       name = "Papirus-Dark";
       package = pkgs.catppuccin-papirus-folders.override {
         flavor = "mocha";
-        accent = "lavender";
+        accent = "pink";
       };
+    };
+
+    gtk3.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
+
+    gtk4.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
     };
   };
 }
