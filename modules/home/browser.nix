5a4184e91a77a737035099016c5c7edcdc9cddbf
me1k0r <runner-me1k0r@proton.me>
2025-02-10 12:39:31 +0100

    edit: modules/home - browser.nix
    typo

--- modules/home/browser.nix
+++ modules/home/browser.nix
@@ -5,7 +5,7 @@
     with pkgs;
     [
       (
-        if (host == "onyx") then
+        if (hostname == "onyx") then
           inputs.zen-browser.packages."${system}".generic
         else
           inputs.zen-browser.packages."${system}".specific
