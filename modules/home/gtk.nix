2e639c2eb40cf7116f502a5d12d5f02989b071d4
me1k0r <runner-me1k0r@proton.me>
2025-02-10 13:32:11 +0100

    edit: modules/home - gtk.nix
    removed commented lines

--- modules/home/gtk.nix
+++ modules/home/gtk.nix
@@ -5,7 +5,7 @@
   catppuccin = {
     gtk = {
       enable = true;
-      gnomeShell = true;
+      gnomeShellTheme = true;
       icon.enable = true;
     };
 
@@ -21,25 +21,7 @@
       name = "JetBrainsMono Nerd Font";
       size = 12;
     };
-    # theme = {
-    #   name = "Catppuccin-Mocha";
-    #   package = pkgs.magnetic-catppuccin-gtk.override {
-    #     shade = "dark";
-    #     size = "standard";
-    #   };
-    # };
-    # iconTheme = {
-    #   name = "Papirus-Dark";
-    #   package = pkgs.catppuccin-papirus-folders.override {
-    #     flavor = "mocha";
-    #     accent = "mauve";
-    #   };
-    # };
     gtk3.extraConfig.gtk-application-prefer-dark-theme = true;
     gtk4.extraConfig.gtk-application-prefer-dark-theme = true;
   };
-
-  # home.sessionVariables = {
-  #   GTK_THEME = "Catppuccin-Mocha";
-  # };
 }
