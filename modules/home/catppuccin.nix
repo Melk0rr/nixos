1e8a5fde68ba73051280caa138ea6b815f438ec6
me1k0r <runner-me1k0r@proton.me>
2025-02-10 13:32:41 +0100

    edit: modules/home - catppuccin.nix
    some more usages

--- modules/home/catppuccin.nix
+++ modules/home/catppuccin.nix
@@ -7,8 +7,11 @@
 
     kitty.enable = true;
     yazi.enable = true;
-
     fish.enable = true;
+    lazygit.enable = true;
+
+    kvantum.enable = true;
     mpv.enable = true;
+    imv.enable = true;
   };
 }
