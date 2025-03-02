e3f425d77cf1e02135b597f2b570b50ade4d8e54
me1k0r <runner-me1k0r@proton.me>
2025-03-02 21:29:17 +0100

    new: modules/home - ghostty
    Ghostty terminal module

---
+++ modules/home/ghostty/default.nix
@@ -0,0 +1,43 @@
+{ ... }:
+
+# Ghostty config 
+{
+  programs.ghostty = {
+    enabled = true;
+    enableFishIntegration = true;
+    settings = {
+      # Fonts
+      font-family = "JetBrains Mono Nerd Font";
+      font-size = 12;
+      adjust-underline-position = 4;
+      adjust-underline-thickness = "130%";
+      theme = "catppuccin-mocha";
+
+      # Padding
+      window-padding-x = 20;
+      window-padding-y = 10;
+
+      # Cursor
+      shell-integration-features = "cursor";
+
+      # Tabs
+      gtk-wide-tabs = false;
+      gtk-tabs-location = "bottom";
+      gtk-custom-css = "tab-style.css";
+
+      keybind = [
+        # Split navigation
+        "shift+ctrl+h=new_split:left"
+        "shift+ctrl+l=new_split:right"
+        "shift+ctrl+k=new_split:up"
+        "shift+ctrl+j=new_split:down"
+
+        "ctrl+a>h=goto_split:left"
+        "ctrl+a>l=goto_split:right"
+        "ctrl+a>k=goto_split:up"
+        "ctrl+a>j=goto_split:down"
+      ];
+    };
+  };
+}
+
